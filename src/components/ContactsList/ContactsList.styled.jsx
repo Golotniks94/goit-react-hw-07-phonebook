2ef3e721597f85e25fd276229a79b8f000b210af
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-03-21 22:55:14 +0200

    First push!

---
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: relative;
+  flex-grow: 1;
+  padding: 20px 40px;
+  background-color: rgb(228, 249, 244);
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+export const Empty = styled.p`
+  text-align: center;
+`;
